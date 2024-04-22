93735a7c3cc1d9463e39a583c886550ee626c64d
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-04-23 03:42:30 +0600

    upload data - read file - converst to json if it is csv

--- upload_data/views.py
+++ upload_data/views.py
@@ -2,25 +2,30 @@ from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
 
+import pandas as pd
+
 
 
 def index(request):
     if request.method == 'POST':
+        company_name = request.POST.get('company')
         file = request.FILES.get('file')
 
+        if not company_name or not file:
+            context = {
+                'title': 'Home | Stock Price',
+            }
+
+            template = loader.get_template('index.html')
+
+            return HttpResponse(template.render(context, request))
+
         if file and file.content_type == 'text/csv':
-            print('format...')
-
-        # # Read the uploaded file into a Pandas DataFrame
-        # import pandas as pd
-        # df = pd.read_csv(uploaded_file)
-
-        # # Convert DataFrame to JSON
-        # json_data = df.to_json(orient="records")
-
-        # # Save JSON data to a file (e.g., "uploaded_data.json")
-        # with open("uploaded_data.json", "w") as json_file:
-        #     json_file.write(json_data)
+            df = pd.read_csv(file)
+            json_data = df.to_json(orient="records")
+        
+            with open(f"data_collection/{company_name}.json", "w") as json_file:
+                json_file.write(json_data)
 
     context = {
         'title': 'Home | Stock Price',
