61a1c5a82bed1d2f670c8360a86c3f575a2274c2
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-04-23 03:30:21 +0600

    upload data - file read - wip

--- upload_data/views.py
+++ upload_data/views.py
@@ -5,6 +5,23 @@ from django.http import HttpResponse
 
 
 def index(request):
+    if request.method == 'POST':
+        file = request.FILES.get('file')
+
+        if file and file.content_type == 'text/csv':
+            print('format...')
+
+        # # Read the uploaded file into a Pandas DataFrame
+        # import pandas as pd
+        # df = pd.read_csv(uploaded_file)
+
+        # # Convert DataFrame to JSON
+        # json_data = df.to_json(orient="records")
+
+        # # Save JSON data to a file (e.g., "uploaded_data.json")
+        # with open("uploaded_data.json", "w") as json_file:
+        #     json_file.write(json_data)
+
     context = {
         'title': 'Home | Stock Price',
     }
