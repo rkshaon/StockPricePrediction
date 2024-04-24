e60b73d6447ddb7e03b302c9ab2bb67dab3fdaec
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-04-25 03:04:23 +0600

    train model - and display

--- upload_data/views.py
+++ upload_data/views.py
@@ -16,15 +16,15 @@ from keras.layers import LSTM, Dense
 
 
 def index(request):
+    context = {
+        'title': 'Home | Stock Price',
+    }
+
     if request.method == 'POST':
         company_name = request.POST.get('company')
         file = request.FILES.get('file')
 
         if not company_name or not file:
-            context = {
-                'title': 'Home | Stock Price',
-            }
-
             template = loader.get_template('index.html')
 
             return HttpResponse(template.render(context, request))
@@ -40,17 +40,10 @@ def index(request):
                 
         json_file_path = f'data_collection/{company_name}.json'
         df = pd.read_json(json_file_path)
-        
 
         # LSTM
-        # Assuming your data has columns: 'date', 'close'
-        # Convert 'date' column to datetime
         df['date'] = pd.to_datetime(df['date'])
-
-        # Set 'date' as the index
         df.set_index('date', inplace=True)
-
-        # Normalize the data
         scaler = MinMaxScaler()
         df['close_scaled'] = scaler.fit_transform(df[['close']])
 
@@ -62,9 +55,8 @@ def index(request):
             y.append(df['close_scaled'].iloc[i+sequence_length])
 
         X, y = np.array(X), np.array(y)
-
-        # Split data into train and test sets
         train_size = int(0.8 * len(X))
+
         X_train, y_train = X[:train_size], y[:train_size]
         X_test, y_test = X[train_size:], y[train_size:]
 
@@ -79,95 +71,41 @@ def index(request):
 
         # Evaluate the model on test data
         test_loss = model.evaluate(X_test, y_test)
-        print(f"Test Loss: {test_loss:.4f}")
+        # print(f"Test Loss: {test_loss:.4f}")
 
         # Make predictions
         predictions = model.predict(X_test)
         predicted_prices = scaler.inverse_transform(predictions)
 
-        # # Plot actual vs. predicted prices
-        # plt.figure(figsize=(10, 6))
-        # plt.plot(df.index[-len(y_test):],
-        #         df['close'].iloc[-len(y_test):], label='Actual Prices')
-        # plt.plot(df.index[-len(y_test):], predicted_prices,
-        #         label='Predicted Prices', color='red')
-        # plt.xlabel('Date')
-        # plt.ylabel('Stock Price')
-        # plt.title('Stock Price Prediction using LSTM')
-        # plt.legend()
-        # plt.show()
-
-        # ARIMA
-        # df['date'] = pd.to_datetime(df['date'])
-        # df.set_index('date', inplace=True)
-        # train_size = int(0.8 * len(df))
-        # train_data, test_data = df[:train_size], df[train_size:]
+        # print(predicted_prices)
+        # print(type(predicted_prices))
+        print(predicted_prices[0][0])
+        print(predicted_prices[1][0])
+        print(predicted_prices[2][0])
+        print(predicted_prices[3][0])
+        print(predicted_prices[4][0])
         
-        # # arima model
-        # model = ARIMA(train_data['close'], order=(5, 1, 0))
-        # model_fit = model.fit()
+        data = []
+        last_date = df.index[0]
+        print(last_date, type(last_date))
 
-        # # Make predictions
-        # predictions = model_fit.forecast(steps=len(test_data))
-        # test_data['Predictions'] = predictions
-        # rmse = np.sqrt(
-        #     np.mean((test_data['close'] - test_data['Predictions'])**2))
+        for i in range(0, 5):
+            # data[last_date + pd.Timedelta(days=(i+1))] = predicted_prices[i][0]
+            data.append({
+                'date': (last_date + pd.Timedelta(days=(i+1))).date(),
+                'close': round(predicted_prices[i][0], 2)
+            })
+            # last_date = last_date + pd.Timedelta(days=(i+1))
 
+        print(data)
+        context['predicted_data'] = data
+        context['company_name'] = company_name
 
-        # # # Plot actual vs. predicted prices
-        # # plt.figure(figsize=(10, 6))
-        # # plt.plot(train_data.index, train_data['close'], label='Train Data')
-        # # plt.plot(test_data.index, test_data['close'], label='Test Data')
-        # # plt.plot(test_data.index, test_data['Predictions'],
-        # #         label='Predictions', color='red')
-        # # plt.xlabel('Date')
-        # # plt.ylabel('Stock Price')
-        # # plt.title('Stock Price Prediction using ARIMA')
-        # # plt.legend()
-        # # plt.show()
-
-        # # # Get next 3 days stock price prediction using the trained ARIMA model
-        # # forecast_steps = 3
-        # # forecast, stderr, conf_int = model_fit.forecast(steps=forecast_steps)
-
-        # # # Print the predictions for the next 3 days
-        # # for i in range(forecast_steps):
-        # #     print(f"Day {i+1}: Predicted Price = {forecast[i]:.2f}")
-
-        # # # Plot the next 3 days predictions
-        # # plt.figure(figsize=(10, 6))
-        # # plt.plot(test_data.index, test_data['close'], label='Test Data')
-        # # plt.plot(test_data.index[-1:] + pd.DateOffset(days=1),
-        # #         forecast, label='Next 3 Days Predictions', color='red')
-        # # plt.xlabel('Date')
-        # # plt.ylabel('Stock Price')
-        # # plt.title('Next 3 Days Stock Price Prediction using ARIMA')
-        # # plt.legend()
-        # # plt.show()
-
-
-        # # # Get next 3 days stock price prediction using the trained ARIMA model
-        # # forecast_steps = 3
-        # # forecast, stderr, conf_int = model_fit.forecast(steps=forecast_steps)
-
-        # # # Print the predictions for the next 3 days
-        # # for i in range(forecast_steps):
-        # #     print(f"Day {i+1}: Predicted Price = {forecast}")
-
-        # # # Plot the next 3 days predictions
-        # # plt.figure(figsize=(10, 6))
-        # # plt.plot(test_data.index, test_data['close'], label='Test Data')
-        # # plt.plot(test_data.index[-1:] + pd.DateOffset(days=1),
-        # #         forecast, label='Next 3 Days Predictions', color='red')
-        # # plt.xlabel('Date')
-        # # plt.ylabel('Stock Price')
-        # # plt.title('Next 3 Days Stock Price Prediction using ARIMA')
-        # # plt.legend()
-        # # plt.show()
-
-    context = {
-        'title': 'Home | Stock Price',
-    }
+    # context = {
+    #     'title': 'Home | Stock Price',
+    #     # 'predicted_data': data,
+    #     # 'company_name': company_name,
+    # }
 
     template = loader.get_template('index.html')
 
