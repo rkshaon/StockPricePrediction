05e62a664a7b50fe3b08eded37041ad91637140d
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-04-29 01:38:50 +0600

    Prophet, LSTM model - both applied

--- upload_data/views.py
+++ upload_data/views.py
@@ -39,11 +39,12 @@ def index(request):
         
             with open(f"data_collection/{company_name}.json", "w") as json_file:
                 json_file.write(json_data)
-                
+
+        context['company_name'] = company_name
+
+        # Prophet        
         json_file_path = f'data_collection/{company_name}.json'
         df = pd.read_json(json_file_path)
-        
-        # Prophet
         last_date = df.iloc[0]['date'].date()
         # Prepare the data
         df.rename(columns={'date': 'ds', 'close': 'y'}, inplace=True)
@@ -70,62 +71,64 @@ def index(request):
 
             sd += datetime.timedelta(days=1)
             
-        context['predicted_data'] = data
-        context['company_name'] = company_name
+        context['prophet_predicted_data'] = data
 
-        # # LSTM
-        # df['date'] = pd.to_datetime(df['date'])
-        # df.set_index('date', inplace=True)
-        # scaler = MinMaxScaler()
-        # df['close_scaled'] = scaler.fit_transform(df[['close']])
+        # LSTM
+        json_file_path = f'data_collection/{company_name}.json'
+        df = pd.read_json(json_file_path)
+        df['date'] = pd.to_datetime(df['date'])
+        df.set_index('date', inplace=True)
+        scaler = MinMaxScaler()
+        df['close_scaled'] = scaler.fit_transform(df[['close']])
 
-        # # Create sequences for training
-        # sequence_length = 60  # Number of days to look back
-        # X, y = [], []
-        # for i in range(len(df) - sequence_length):
-        #     X.append(df['close_scaled'].iloc[i:i+sequence_length])
-        #     y.append(df['close_scaled'].iloc[i+sequence_length])
+        # Create sequences for training
+        sequence_length = 60  # Number of days to look back
+        X, y = [], []
+        for i in range(len(df) - sequence_length):
+            X.append(df['close_scaled'].iloc[i:i+sequence_length])
+            y.append(df['close_scaled'].iloc[i+sequence_length])
 
-        # X, y = np.array(X), np.array(y)
-        # train_size = int(0.8 * len(X))
+        X, y = np.array(X), np.array(y)
+        train_size = int(0.8 * len(X))
 
-        # X_train, y_train = X[:train_size], y[:train_size]
-        # X_test, y_test = X[train_size:], y[train_size:]
+        X_train, y_train = X[:train_size], y[:train_size]
+        X_test, y_test = X[train_size:], y[train_size:]
 
-        # # Build the LSTM model
-        # model = Sequential()
-        # model.add(LSTM(50, input_shape=(sequence_length, 1)))
-        # model.add(Dense(1))
-        # model.compile(optimizer='adam', loss='mean_squared_error')
+        # Build the LSTM model
+        model = Sequential()
+        model.add(LSTM(50, input_shape=(sequence_length, 1)))
+        model.add(Dense(1))
+        model.compile(optimizer='adam', loss='mean_squared_error')
 
-        # # Train the model
-        # model.fit(X_train, y_train, epochs=10, batch_size=32)
+        # Train the model
+        model.fit(X_train, y_train, epochs=10, batch_size=32)
 
-        # # Evaluate the model on test data
-        # test_loss = model.evaluate(X_test, y_test)
-        # # print(f"Test Loss: {test_loss:.4f}")
+        # Evaluate the model on test data
+        test_loss = model.evaluate(X_test, y_test)
+        # print(f"Test Loss: {test_loss:.4f}")
 
-        # # Make predictions
-        # predictions = model.predict(X_test)
-        # predicted_prices = scaler.inverse_transform(predictions)
+        # Make predictions
+        predictions = model.predict(X_test)
+        predicted_prices = scaler.inverse_transform(predictions)
         
-        # print(predicted_prices[0][0])
-        # print(predicted_prices[1][0])
-        # print(predicted_prices[2][0])
-        # print(predicted_prices[3][0])
-        # print(predicted_prices[4][0])
-        
-        # data = []
-        # last_date = df.index[0]
+        data = []
+        last_date = df.index[0]
+        sd = last_date + pd.Timedelta(days=1)
+        ed = sd + datetime.timedelta(days=7)
+        weekends = {4, 5}
+        i = 0
 
-        # for i in range(0, 5):
-        #     data.append({
-        #         'date': (last_date + pd.Timedelta(days=(i+1))).date(),
-        #         'close': round(predicted_prices[i][0], 2),
-        #     })
+        while sd != ed:
+            if sd.weekday() not in weekends:
+                data.append({
+                    'date': sd,
+                    'close': round(predicted_prices[i][0], 2),
+                })
+
+            i += 1
+            sd += datetime.timedelta(days=1)
             
-        # context['predicted_data'] = data
-        # context['company_name'] = company_name
+        context['lstm_predicted_data'] = data
 
     template = loader.get_template('index.html')
 
